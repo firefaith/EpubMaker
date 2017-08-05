fa2e3888649a4e13011367bd8570b69ed152c2ea
Chunying Gu <firefaith@126.com>
2017-08-05 15:17:57 +0800

    testBuild.py

--- testBuild.py
+++ testBuild.py
@@ -2,10 +2,16 @@
 #coding:utf-8
 import ConfigParser
 from lib import const
+from lib.category import Category
+import os
+import shutil 
 
-cf = ConfigParser.ConfigParser()
 conf_file = 'book.conf'
-cate_file = 'testbooke_cate.txt'
+cate_file = 'testbook_cate.txt'
+output_dir = 'tmp/doc'
+
+# read configuration file
+cf = ConfigParser.ConfigParser()
 cf.read(conf_file)
 cf.set(const.CONF_NAME,const.BOOK_NAME,'TestBook')
 cf.set(const.CONF_NAME,const.CONTENT_PATH,'testbook')
@@ -21,6 +27,13 @@ with open(conf_file,"w+") as f:
 
 print const.BOOK_NAME
 
+if os.path.exists(output_dir)==False:
+    os.makedirs(output_dir)
+else:
+    shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+
+# write category file
 with open(cate_file,"w") as f:
 	for i in range(3):
 		f.write("title_"+ str(i+1)+'\n')
@@ -28,5 +41,20 @@ with open(cate_file,"w") as f:
 			f.write("\tsubtitle_"+str(i+1)+"."+str(j+1)+'\n')
 			if(j==1):
 				f.write("\t\ttrd_subtitle_"+str(i+1)+"."+str(j+1)+'.1\n')
+cate = Category()
+cate.readCate(cate_file)
+cate.printCate()
 
+# write test content file
+def writeContent(cate):
+    if cate.hasSub:
+      for c in cate.getSubCate():
+        writeContent(c)
+    else: 
+        outfile_path = output_dir+'/'+cate.title+".txt"
+        print outfile_path
+        with open(outfile_path,'w') as f:
+          f.write("file path:"+outfile_path)
+          f.close()
 
+writeContent(cate)
